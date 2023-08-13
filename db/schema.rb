cf80c00344699dea8da30e503a2162d3018294f1
kushk612 <kushk612@gmail.com>
2023-08-13 16:26:33 +0530

    add the migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_11_073129) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_13_105127) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_11_073129) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "slug"
+    t.string "test"
   end
 
   create_table "events", force: :cascade do |t|
